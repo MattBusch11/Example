3907e529f4ac3576d00f83bb3fd75e5e6452cfed
Matt Busch <mbb23738@email.vccs.edu>
2017-09-05 16:37:45 -0400

    initial commit

---
+++ HelloMyName.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloMyName {
+    public static void main(String [] args) {
+        String username, password;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter username");
+        username = keyboard.nextLine();
+        System.out.println("Enter password");
+        password = keyboard.nextLine();
+        System.out.println("Hello " + username + ", Welcome to CSC200 class! And your password is " + password);
+        keyboard.close();
+    }
+}
